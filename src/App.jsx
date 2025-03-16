ef68e2f96b0599891429916a4f1a4e5519ba6cd0
Jonah Bitautas <jonahvsweb@users.noreply.github.com>
2025-03-15 21:14:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ComponentOne from "./components/ComponentOne";
+
+const App = () => {
+  return (
+    <main>
+      <section>
+        <h1>Main</h1>
+      </section>
+      <ComponentOne />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/ComponentOne.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const ComponentOne = () => {
+  return (
+    <section>
+      <h3>ComponentOne</h3>
+    </section>
+  );
+};
+
+export default ComponentOne;
